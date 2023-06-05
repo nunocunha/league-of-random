1748f96e8d0091afc37585c9e254860b387a6cad
Nuno Cunha <me@nunocunha.pt>
2023-06-05 17:55:28 +0100

    Add role component

---
+++ src/app/components/role/role.component.ts
@@ -0,0 +1,24 @@
+import {CommonModule} from '@angular/common';
+import {Component, OnDestroy} from '@angular/core';
+
+import {RoleSelectService} from './services/role-select/role-select.service';
+
+@Component({
+  selector: 'lr-role',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './role.component.html',
+  styleUrls: ['./role.component.css']
+})
+export class RoleComponent implements OnDestroy {
+  public readonly role: string;
+
+  public constructor(private roles: RoleSelectService) {
+    this.role = this.roles.random();
+    this.roles.select(this.role);
+  }
+
+  public ngOnDestroy() {
+    this.roles.release(this.role);
+  }
+}
