095243f789afe182ede80ea61364f05348d750ad
Nuno Cunha <me@nunocunha.pt>
2023-06-05 17:55:04 +0100

    Add champion component

---
+++ src/app/components/champion/champion.component.ts
@@ -0,0 +1,28 @@
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {Component, OnDestroy} from '@angular/core';
+
+import {Champion} from './services/champion-data/models/internal/champion';
+import {ChampionSelectService} from './services/champion-select/champion-select.service';
+
+@Component({
+  selector: 'lr-champion',
+  standalone: true,
+  imports: [
+    CommonModule,
+    NgOptimizedImage
+  ],
+  templateUrl: './champion.component.html',
+  styleUrls: ['./champion.component.css']
+})
+export class ChampionComponent implements OnDestroy {
+  public champion: Champion;
+
+  public constructor(private champions: ChampionSelectService) {
+    this.champion = this.champions.random();
+    this.champions.select(this.champion);
+  }
+
+  public ngOnDestroy() {
+    this.champions.release(this.champion);
+  }
+}
